bd3998e5df68e6dfc60377fb87aa0bc9fcc52b6c
viDaLost <vitaledanilov@gmail.com>
2026-01-24 15:46:42 +0300

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,5 @@
+// supabase-config.js
+// Вставь сюда данные из Supabase Project Settings → API
+
+window.SUPABASE_URL = "https://ajqsldfiphchponkgvih.supabase.co";
+window.SUPABASE_ANON_KEY = "sb_publishable_fihl6ELd3CU0AQNbrmc_mQ_SSbve-NK";
