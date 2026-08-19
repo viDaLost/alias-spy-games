78d6090a6b618d726d87e28f519d0a2a185f8bd0
viDaLost <vitaledanilov@gmail.com>
2026-08-19 18:59:47 +0300

    Reuse existing V7.3.11 renderer bridge

--- web/games/moses-nile-v7/js/v7312-terrain-bridge.js
+++
@@ -1,7 +0,0 @@
-(()=>{
-  'use strict';
-  if(window.__mosesV7312Bridge||!window.THREE)return;window.__mosesV7312Bridge=true;
-  const NativeRenderer=THREE.WebGLRenderer,NativeCamera=THREE.PerspectiveCamera;
-  THREE.WebGLRenderer=class MosesRendererBridge extends NativeRenderer{constructor(...args){super(...args);window.__mosesRenderer=this;}};
-  THREE.PerspectiveCamera=class MosesCameraBridge extends NativeCamera{constructor(...args){super(...args);window.__mosesCamera=this;}};
-})();
