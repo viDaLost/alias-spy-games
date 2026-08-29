ce637474f7d265ecc1e378240c0ae16d251a3af9
viDaLost <vitaledanilov@gmail.com>
2026-08-29 11:28:15 +0300

    fix(android): align game catalog copy with web

--- android-app/app/src/main/java/com/vidalost/biblegames/model/Models.kt
+++ android-app/app/src/main/java/com/vidalost/biblegames/model/Models.kt
@@ -13,13 +13,13 @@ enum class GameKey(
     GUESS("guess", "Угадай персонажа", "Вопросы, версии, логика", "assets/icons/character.png", GameSection.COMPANY, 0xFF2563EB),
     DESCRIBE("describe", "Опиши, но не называй", "Подсказки без прямого ответа", "assets/icons/describe.png", GameSection.COMPANY, 0xFFF97316),
     SPY("spy", "Шпион", "Секретная роль и локация", "assets/icons/spy.png", GameSection.COMPANY, 0xFF4338CA),
-    QUARTET("quartet", "Квартет", "Собери четыре карты", "assets/icons/quartet.png", GameSection.COMPANY, 0xFF0F766E),
+    QUARTET("quartet", "Квартет", "Собери четыре связанные карты", "assets/icons/quartet.png", GameSection.COMPANY, 0xFF0F766E),
     WOW("bible-wow", "Библейские слова", "Собери слова из букв", "assets/icons/words.png", GameSection.WORDS, 0xFF7C3AED),
     WORD_SEARCH("bible-wordsearch", "Поиск библейских слов", "Найди скрытые слова", "assets/icons/search.png", GameSection.WORDS, 0xFF0284C7),
     SACRED("sacred-word", "Священное слово", "Открой слово по подсказкам", "assets/icons/sacred.png", GameSection.WORDS, 0xFFB7791F),
-    PAIRS("kids-ark-pairs", "Найди пару", "Память, пары и ковчег", "assets/icons/ark.png", GameSection.KIDS, 0xFF059669),
-    MATCH_THREE("biblical-match-three", "Библейские сокровища", "Матч‑3, каскады и Путь света", "assets/icons/biblical-treasures-v38.png", GameSection.KIDS, 0xFF4F46E5),
-    SKETCH("bible-sketch", "Библейский художник", "Рисуйте вместе и найдите шпиона", "assets/icons/bible-sketch.webp", GameSection.COMPANY, 0xFFDB2777),
+    PAIRS("kids-ark-pairs", "Найди пару", "Соберите животных попарно", "assets/icons/ark.png", GameSection.KIDS, 0xFF059669),
+    MATCH_THREE("biblical-match-three", "Библейские сокровища", "Комбинации, бустеры и путь из 30 уровней", "assets/icons/biblical-treasures-v38.png", GameSection.KIDS, 0xFF4F46E5),
+    SKETCH("bible-sketch", "Библейский художник", "Рисуйте по очереди и найдите шпиона", "assets/icons/bible-sketch.webp", GameSection.COMPANY, 0xFFDB2777),
     ;
 
     companion object { fun fromRoute(route: String) = entries.firstOrNull { it.route == route } }
@@ -39,9 +39,7 @@ data class PlayerProfile(
 )
 
 data class SacredLevel(val word: String, val hint: String, val category: String)
-
 data class WowLevel(val id: Int, val letters: String, val words: List<String>, val bonus: List<String>)
-
 data class WordSearchLevel(
     val id: Int,
     val theme: String,
